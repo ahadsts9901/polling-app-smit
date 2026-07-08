9672ce52cb3f55a36a348c7c9fb59bdbb7a476ce
ahadsts9901 <ahadsts990@gmail.com>
2026-07-08 15:24:51 +0500

    code

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB40awq88DjoYcRpiCP1svcdCE3QWyLEcA",
-    authDomain: "react-firebase-smit-e66c7.firebaseapp.com",
-    projectId: "react-firebase-smit-e66c7",
-    storageBucket: "react-firebase-smit-e66c7.firebasestorage.app",
-    messagingSenderId: "927231985259",
-    appId: "1:927231985259:web:bd469d44f2ed708d05932e",
-    measurementId: "G-CS9VTFQVMY"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
